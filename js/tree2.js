a148ae376084a5ac003b7117fdde70272ffa321b
Oliver Warden <oliverwarden@gmail.com>
2019-04-02 21:08:06 +0100

    single treemap with slider

--- js/tree2.js
+++ js/tree2.js
@@ -1,19 +1,29 @@
-function drawTree(){
-    var width = 500
-    let height = 500
-    let margin = {top: 20, bottom:20, left:20, right:20}
-    let svg = d3.select("#scatterVis")
+var t2Width = 500
+let t2Height = 500
+var rawData = d3.csv('/finalData/tree2.csv')
+var tree = d3.select("#treeVis")
+    .append("g")
+tree.append("div")
+    .attr("id", "treeLabel")
+var t2Svg = tree.append("svg")
+    .attr("width", t2Width)
+    .attr("height", t2Height)
+var t2TTip = tree.append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0)
+
+function tree2Update(){
+    
     var colourKey = {"Injuries": "#66c2a5",
         "Non-communicable diseases": "#fc8d62",
         "Communicable, maternal, neonatal, and nutritional diseases":"#8da0cb"}
+    var t = d3.transition()
+        .duration(800)
 
-    var rawData = d3.csv('/finalData/tree2.csv')
+
     rawData.then(function(data){
         data.forEach(function(d) {
             d.GBD_val = +d.GBD_val
-        })
-        data = data.filter(function(d){
-                return (d.location_name == "World Bank High Income" || d.location_name == "World Bank Low Income")
         })
         treeData = d3.nest()
             .key(function(d){ return d.location_name})
@@ -22,47 +32,54 @@ function drawTree(){
             .rollup(function(d){ return d3.sum(d, function(d) { return d.GBD_val})})
             .entries(data)
 
-        //draw all the treemaps
-        for(i = 0; i <2; i++){
-            console.log(i)
-            var hroot = d3.hierarchy(treeData[i], function(d) { return d.values})
-                .sum(function(d){return d.value})
+        selectedTree = document.getElementById("incomeSlider").value
+        var hroot = d3.hierarchy(treeData[selectedTree], function(d) { return d.values})
+            .sum(function(d){return d.value})
 
-            var treeLayout = d3.treemap()
-                .size([width, height])
-                .padding(1)
-            treeLayout(hroot)
+        var treeLayout = d3.treemap()
+            .size([t2Width, t2Height])
+            .padding(1)
+        treeLayout(hroot)
+        
+        console.log(hroot)
+        tree.select("#treeLabel")
+           .text(hroot.data.key)
 
-            //console.log(hroot.descendants())
-            var tree = d3.select("#treeVis")
-                .append("g")
-            
-            console.log(hroot)
-            tree.append("div")    
-                .text(hroot.data.key)
-            tree.append("svg")
-                    .attr("width", width)
-                    .attr("height", height)
-                .selectAll("tree")
-                    .data(hroot.leaves())
-                    //.append("text")
-                    //.attr("text", d => d.data.key)
-                    .enter()
+        var nodes = t2Svg.selectAll(".treeNode")
+                .data(hroot.leaves())
+
+        console.log(nodes)
+        var new_nodes = nodes.enter()
                 .append("rect")
-                    .attr("x", function(d) { return d.x0})
-                    .attr("y", function(d) { return d.y0})
-                    .attr("width", function(d){ return d.x1 - d.x0})
-                    .attr("height", function(d) {return d.y1 - d.y0})
-                    .attr("text", function(d){ return d.data.key})
-                    .attr("fill", function(d){ return colourKey[d.parent.data.key]})
+                .attr("class", "treeNode")
+        
+        // new_nodes.append("text")
+            // .html(d => d.data.key)
+
+        nodes.exit()
+            .remove()
+                
+        new_nodes.merge(nodes)
+            .on("mouseover", function(d){
+                t2TTip.html(d.data.key)
+                    .style("opacity", 1)
+                    .style("left", d3.event.pageX + 10 +"px")
+                    .style("top", d3.event.pageY + "px")
+            })
+            .on("mouseout", function(d){
+                t2TTip.style("opacity", 0)
+            })
+            .transition(t)
+            .attr("x", function(d) { return d.x0})
+            .attr("y", function(d) { return d.y0})
+            .attr("width", function(d){ return d.x1 - d.x0})
+            .attr("height", function(d) {return d.y1 - d.y0})
+            .attr("text", function(d){ return d.data.key})
+            .attr("fill", function(d){ return colourKey[d.parent.data.key]})
+            
                  
 
-        }
+        
     })
-
-
-
-
-
 }
-drawTree();
+tree2Update();
