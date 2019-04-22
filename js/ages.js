f761e3fde09a92c94ba7aace925f69b9e85ea7b4
Oliver Warden <zarks1@gmail.com>
2019-04-22 21:51:01 +0100

    colour bars and set centre width properly

--- js/ages.js
+++ js/ages.js
@@ -2,6 +2,7 @@ function drawVis(){
     let width = 700
     let height = 700;
     let margin = {top: 20, bottom:20, left:20, right:20}
+    let centreWidth = 72
     let svg = d3.select("#ageVis")
         .append("svg")
             .attr("height", height)
@@ -13,27 +14,29 @@ function drawVis(){
             d.percent_dying = +d.percent_dying
         })
         dieScale = d3.scaleLinear()
-            .range([0, width/2 - 100])
+            .range([0, width/2 - centreWidth])
             .domain([0, d3.max(data, d=>d.percent_dying)])
         aliveScale = d3.scaleLinear()
-            .range([0, width/2 - 100])
+            .range([0, width/2 - centreWidth])
             .domain([0, d3.max(data, d=> d.percent_alive)])
         ageScale = d3.scaleBand()
             .range([0,height])
             .domain(d3.range(23))
             .padding(0.05)
-            
+
         rightData = data.filter(d => d.location_name == "World Bank High Income")
         rightBars = svg.selectAll(".rightBar")
             .data(rightData)
             .enter()
             .append("rect")
             .attr("class","rightBar")
-            .attr("x",width/2 + 50)
+            .attr("x",width/2 + centreWidth/2)
             .attr("width", d => dieScale(d.percent_dying))
             .attr("y", function(d,i){ 
                 return ageScale(i)})
             .attr("height", ageScale.bandwidth())
+            .attr("fill", wbAreaColours["World Bank High Income"])
+            .attr("opacity","0.8")
 
         leftData = data.filter(d => d.location_name == "World Bank Low Income")
         leftBars = svg.selectAll(".leftBar")
@@ -42,12 +45,14 @@ function drawVis(){
             .append("rect")
             .attr("class","leftBar")
             .attr("x", function(d){
-                return width/2 - 50 - dieScale(d.percent_dying)
+                return width/2 - centreWidth/2 - dieScale(d.percent_dying)
             })
             .attr("width", d => dieScale(d.percent_dying))
             .attr("y", function(d,i){ 
                 return ageScale(i)})
             .attr("height", ageScale.bandwidth())
+            .attr("fill", wbAreaColours["World Bank Low Income"])
+            .attr("opacity","0.8")
 
 
     })
