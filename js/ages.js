0730eb4f40fa7acd0d8347f4a8296f2a03502a72
Oliver Warden <zarks1@gmail.com>
2019-04-22 22:09:36 +0100

    add centre labels

--- js/ages.js
+++ js/ages.js
@@ -2,7 +2,7 @@ function drawVis(){
     let width = 700
     let height = 700;
     let margin = {top: 20, bottom:20, left:20, right:20}
-    let centreWidth = 72
+    let centreWidth = 60
     let svg = d3.select("#ageVis")
         .append("svg")
             .attr("height", height)
@@ -21,7 +21,7 @@ function drawVis(){
             .domain([0, d3.max(data, d=> d.percent_alive)])
         ageScale = d3.scaleBand()
             .range([0,height])
-            .domain(d3.range(23))
+            .domain(d3.range(23)) //23 age categories
             .padding(0.05)
 
         rightData = data.filter(d => d.location_name == "World Bank High Income")
@@ -29,32 +29,39 @@ function drawVis(){
             .data(rightData)
             .enter()
             .append("rect")
-            .attr("class","rightBar")
-            .attr("x",width/2 + centreWidth/2)
-            .attr("width", d => dieScale(d.percent_dying))
-            .attr("y", function(d,i){ 
-                return ageScale(i)})
-            .attr("height", ageScale.bandwidth())
-            .attr("fill", wbAreaColours["World Bank High Income"])
-            .attr("opacity","0.8")
+                .attr("class","rightBar")
+                .attr("x",width/2 + centreWidth/2)
+                .attr("width", d => dieScale(d.percent_dying))
+                .attr("y", function(d,i){ 
+                    return ageScale(i)})
+                .attr("height", ageScale.bandwidth())
+                .attr("fill", wbAreaColours["World Bank High Income"])
+                .attr("opacity","0.8")
 
         leftData = data.filter(d => d.location_name == "World Bank Low Income")
         leftBars = svg.selectAll(".leftBar")
             .data(leftData)
             .enter()
             .append("rect")
-            .attr("class","leftBar")
-            .attr("x", function(d){
-                return width/2 - centreWidth/2 - dieScale(d.percent_dying)
-            })
-            .attr("width", d => dieScale(d.percent_dying))
-            .attr("y", function(d,i){ 
-                return ageScale(i)})
-            .attr("height", ageScale.bandwidth())
-            .attr("fill", wbAreaColours["World Bank Low Income"])
-            .attr("opacity","0.8")
+                .attr("class","leftBar")
+                .attr("x", function(d){
+                    return width/2 - centreWidth/2 - dieScale(d.percent_dying)
+                })
+                .attr("width", d => dieScale(d.percent_dying))
+                .attr("y", function(d,i){ 
+                    return ageScale(i)})
+                .attr("height", ageScale.bandwidth())
+                .attr("fill", wbAreaColours["World Bank Low Income"])
+                .attr("opacity","0.8")
 
-
+        centreLabels = svg.selectAll(".ageLabel")
+            .data(leftData)
+            .enter()
+            .append("text")
+            .attr("class","ageLabel")
+            .attr("x",width/2)
+            .attr("y", function(d,i){return ageScale(i) + 18})
+            .text(d => d.age_group_name)
     })
 
 }drawVis()
