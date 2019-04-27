f47ea6e41a81f5f87fcae10afb3e7a8890ee9af8
Oliver Warden <zarks1@gmail.com>
2019-04-27 12:34:04 +0100

    still alive label

--- js/ages.js
+++ js/ages.js
@@ -1,17 +1,17 @@
 d3.select(window)
     .on("scroll", checkScrollPosition)
     
+let ageMargin = {top: 10, bottom:10, left:60, right:60}
+let ageWidth = 800
+let ageHeight = 600;
+
 function checkScrollPosition(){
 	var pagePos = window.pageYOffset
-    var agePos = document.getElementById("ageVis").getBoundingClientRect().top
+    var agePos = document.getElementById("ageVis").getBoundingClientRect().top + ageHeight
 	if(agePos < pagePos) {
 		drawAgeVis(pagePos - agePos)
 	}
 }
-let ageMargin = {top: 10, bottom:10, left:10, right:10}
-let ageWidth = 700
-let ageHeight = 600;
-
 var ageSvg = d3.select("#ageVis")
 .append("svg")
     .attr("height", ageHeight)
@@ -21,6 +21,10 @@ var ageSvg = d3.select("#ageVis")
 ageSvg.append("g").attr("id","bars")
 ageSvg.append("g").attr("id","lines")
 ageSvg.append("g").attr("id","centreLabels")
+ageSvg.append("g").attr("id","rightText")
+    .append("text")
+ageSvg.append("g").attr("id","leftText")
+    .append("text")
 ageSvg.append("g").attr("id","mouseBoxes")
 
 ageWidth = ageWidth - ageMargin.left - ageMargin.right;
@@ -55,164 +59,177 @@ function drawAgeVis(scrollPos){
             .padding(0.05)
 
         data = data.filter(d => (d.ageCat < scrollPoint))
+        if(data.length > 2)
+        {
+            //RIGHT BARS
+            rightData = data.filter(d => d.location_name == "World Bank High Income")
+            rightBars = ageSvg.select("#bars").selectAll(".rightBar")
+                .data(rightData.slice(0,rightData.length - 1))
 
-        //RIGHT BARS
-        rightData = data.filter(d => d.location_name == "World Bank High Income")
-        rightBars = ageSvg.select("#bars").selectAll(".rightBar")
-            .data(rightData.slice(0,rightData.length - 1))
-
-        newRightBars = rightBars.enter()
-            .append("g")
-                .attr("transform",function(d,i){
-                    return "translate(" + (ageWidth/2 + centreWidth/2) + "," + ageScale(i) +")"
+            newRightBars = rightBars.enter()
+                .append("g")
+                    .attr("transform",function(d,i){
+                        return "translate(" + (ageWidth/2 + centreWidth/2) + "," + ageScale(i) +")"
+                    })
+                .attr("class","rightBar")
+            newRightBars.append("rect")
+                .attr("height", ageScale.bandwidth())
+                .transition(t)
+                .attr("width", d => dieScale(d.percent_dying))
+                .attr("fill", wbAreaColours["World Bank High Income"])
+                .attr("opacity","0.8")
+            newRightBars.append("text")
+                .text(d => ((d.percent_dying)*100).toPrecision(2)+"%")
+                .attr("class", function(d){
+                    return ("barText"+d.ageCat)
                 })
-            .attr("class","rightBar")
-        newRightBars.append("rect")
-            .attr("height", ageScale.bandwidth())
-            .transition(t)
-            .attr("width", d => dieScale(d.percent_dying))
-            .attr("fill", wbAreaColours["World Bank High Income"])
-            .attr("opacity","0.8")
-        newRightBars.append("text")
-            .text(d => ((d.percent_dying)*100).toPrecision(2)+"%")
-            .attr("class", function(d){
-                return ("barText"+d.ageCat)
-            })
-            .attr("y",18)
-            .attr("x", d => dieScale(d.percent_dying) + 10)
-            .attr("opacity",0)
-            .attr("stroke", wbAreaColours["World Bank High Income"])
+                .attr("y",18)
+                .attr("x", d => dieScale(d.percent_dying) + 10)
+                .attr("opacity",0)
+                .attr("stroke", wbAreaColours["World Bank High Income"])
 
-        rightBars.exit()
-            .remove()
+            rightBars.exit()
+                .remove()
 
-        //LEFT BARS
-        leftData = data.filter(d => d.location_name == "World Bank Low Income")
-        leftBars = ageSvg.select("#bars").selectAll(".leftBar")
-            .data(leftData.slice(0,leftData.length-1))
-            
-        newLeftBars = leftBars.enter()
-            .append("g")
-                .attr("transform",function(d,i){
-                    return "translate(" + 
-                    (ageWidth/2 - centreWidth/2 - dieScale(d.percent_dying)) + 
-                    "," + ageScale(i) +")"
+            //LEFT BARS
+            leftData = data.filter(d => d.location_name == "World Bank Low Income")
+            leftBars = ageSvg.select("#bars").selectAll(".leftBar")
+                .data(leftData.slice(0,leftData.length-1))
+                
+            newLeftBars = leftBars.enter()
+                .append("g")
+                    .attr("transform",function(d,i){
+                        return "translate(" + 
+                        (ageWidth/2 - centreWidth/2 - dieScale(d.percent_dying)) + 
+                        "," + ageScale(i) +")"
+                    })
+                .attr("class","leftBar")
+            newLeftBars.append("rect")
+                .attr("x",d => dieScale(d.percent_dying))
+                .attr("height", ageScale.bandwidth())
+                .transition(t)
+                .attr("x",0)
+                .attr("width", d => dieScale(d.percent_dying))
+                .attr("fill", wbAreaColours["World Bank Low Income"])
+                .attr("opacity","0.8")
+            newLeftBars.append("text")
+                .text(d => ((d.percent_dying)*100).toPrecision(2)+"%")
+                .attr("class", function(d){
+                    return ("barText"+d.ageCat)
                 })
-            .attr("class","leftBar")
-        newLeftBars.append("rect")
-            .attr("x",d => dieScale(d.percent_dying))
-            .attr("height", ageScale.bandwidth())
-            .transition(t)
-            .attr("x",0)
-            .attr("width", d => dieScale(d.percent_dying))
-            .attr("fill", wbAreaColours["World Bank Low Income"])
-            .attr("opacity","0.8")
-        newLeftBars.append("text")
-            .text(d => ((d.percent_dying)*100).toPrecision(2)+"%")
-            .attr("class", function(d){
-                return ("barText"+d.ageCat)
-            })
-            .attr("y",18)
-            .attr("x",-5)
-            .attr("style","text-anchor: end")
-            .attr("opacity",0)
-            .attr("stroke", wbAreaColours["World Bank Low Income"])
-        
-        leftBars.exit()
-            .remove()
-        
-        centreLabels = ageSvg.select("#centreLabels").selectAll(".ageLabel")
-            .data(leftData.slice(0,leftData.length-1))
-        centreLabels.enter()
-            .append("text")
-            .attr("class","ageLabel")
-            .attr("x",ageWidth/2)
-            .attr("y", function(d,i){return ageScale(i) + 18})
-            .text(d => d.age_group_name)
-
-        centreLabels.exit().remove()
+                .attr("y",18)
+                .attr("x",-5)
+                .attr("style","text-anchor: end")
+                .attr("opacity",0)
+                .attr("stroke", wbAreaColours["World Bank Low Income"])
             
-        //LINES
-        var rightLinePath = d3.line()
-            .x(d => aliveScale(d[1]) + ageWidth/2 + centreWidth/2)
-            .y(d => ageScale(d[0]))
-        var leftLinePath = d3.line()
-            .x(d => ageWidth/2 - centreWidth/2 - aliveScale(d[1]))
-            .y(d => ageScale(d[0]))
+            leftBars.exit()
+                .remove()
+            
+            centreLabels = ageSvg.select("#centreLabels").selectAll(".ageLabel")
+                .data(leftData.slice(0,leftData.length-1))
+            centreLabels.enter()
+                .append("text")
+                .attr("class","ageLabel")
+                .attr("x",ageWidth/2)
+                .attr("y", function(d,i){return ageScale(i) + 18})
+                .text(d => d.age_group_name)
 
-        var rightLineData = rightData.map(function(d,i){
+            centreLabels.exit().remove()
+                
+            //LINES
+            var rightLinePath = d3.line()
+                .x(d => aliveScale(d[1]) + ageWidth/2 + centreWidth/2)
+                .y(d => ageScale(d[0]))
+            var leftLinePath = d3.line()
+                .x(d => ageWidth/2 - centreWidth/2 - aliveScale(d[1]))
+                .y(d => ageScale(d[0]))
+
+            var rightLineData = rightData.map(function(d,i){
+                    return [i, d.percent_alive]
+                })
+            var rightLine = ageSvg.select("#lines").selectAll(".rightLine")
+                .data(rightLineData)
+            var newRightLine = rightLine.enter()
+                .append("path")
+                    .attr("class","rightLine")
+                    .attr("fill", "none")
+                    .attr("stroke-width", "2px")
+                    .attr("stroke", "black")
+            newRightLine.merge(rightLine)
+                .attr("d", rightLinePath(rightLineData))
+            rightLine.exit().remove()
+
+            var rightPoint = ageSvg.select("#lines").selectAll("#rightPoint")
+                .data([rightLineData[rightLineData.length - 1]])
+            rightPoint.enter()
+                .append("circle")
+                    .attr("r",4)
+                    .attr("id","rightPoint")
+                    .attr("fill","#000000")
+            rightPoint.attr("cx", d => aliveScale(d[1]) + ageWidth/2 + centreWidth/2)
+                .attr("cy",d=>ageScale(d[0]))
+                
+
+            var leftLineData = leftData.map(function(d,i){
                 return [i, d.percent_alive]
             })
-        var rightLine = ageSvg.select("#lines").selectAll(".rightLine")
-            .data(rightLineData)
-        var newRightLine = rightLine.enter()
-            .append("path")
-                .attr("class","rightLine")
-                .attr("fill", "none")
-                .attr("stroke-width", "2px")
-                .attr("stroke", "black")
-        newRightLine.merge(rightLine)
-            .attr("d", rightLinePath(rightLineData))
-        rightLine.exit().remove()
+            var leftLine = ageSvg.select("#lines").selectAll(".leftLine")
+                .data(leftLineData)
 
-        var rightPoint = ageSvg.select("#lines").selectAll("#rightPoint")
-            .data([rightLineData[rightLineData.length - 1]])
-        rightPoint.enter()
-            .append("circle")
-                .attr("r",4)
-                .attr("id","rightPoint")
-                .attr("fill","#000000")
-        rightPoint.attr("cx", d => aliveScale(d[1]) + ageWidth/2 + centreWidth/2)
-            .attr("cy",d=>ageScale(d[0]))
+            var newLeftLine = leftLine.enter()
+                .append("path")
+                    .attr("class","leftLine")
+                    .attr("fill", "none")
+                    .attr("stroke-width", "2px")
+                    .attr("stroke", "black")
+            newLeftLine.merge(leftLine)
+                .attr("d", leftLinePath(leftLineData))
+            leftLine.exit().remove()
             
-        var leftLineData = leftData.map(function(d,i){
-            return [i, d.percent_alive]
-        })
-        var leftLine = ageSvg.select("#lines").selectAll(".leftLine")
-            .data(leftLineData)
+            var leftPoint = ageSvg.select("#lines").selectAll("#leftPoint")
+                .data([leftLineData[leftLineData.length - 1]])
+            leftPoint.enter()
+                .append("circle")
+                    .attr("r",4)
+                    .attr("id","leftPoint")
+                    .attr("fill","#000000")
+            leftPoint.attr("cx", d => ageWidth/2 - centreWidth/2 - aliveScale(d[1]))
+                .attr("cy",d=>ageScale(d[0]))
 
-        var newLeftLine = leftLine.enter()
-            .append("path")
-                .attr("class","leftLine")
-                .attr("fill", "none")
-                .attr("stroke-width", "2px")
-                .attr("stroke", "black")
-        newLeftLine.merge(leftLine)
-            .attr("d", leftLinePath(leftLineData))
-        leftLine.exit().remove()
-        
-        var leftPoint = ageSvg.select("#lines").selectAll("#leftPoint")
-            .data([leftLineData[leftLineData.length - 1]])
-        leftPoint.enter()
-            .append("circle")
-                .attr("r",4)
-                .attr("id","leftPoint")
-                .attr("fill","#000000")
-        leftPoint.attr("cx", d => ageWidth/2 - centreWidth/2 - aliveScale(d[1]))
-            .attr("cy",d=>ageScale(d[0]))
+            //TEXT
+            ageSvg.select("#rightText").selectAll("text")
+                .data([rightLineData[rightLineData.length - 1]])
+                .attr("x", ageWidth)
+                .attr("y",d=>ageScale(d[0]))
+                .text(d => (d[1]*100).toPrecision(2)+"%")
+            ageSvg.select("#leftText").selectAll("text")
+                .data([leftLineData[leftLineData.length - 1]])
+                .attr("x", 0)
+                .attr("y",d=>ageScale(d[0]))
+                .text(d => (d[1]*100).toPrecision(2)+"%")
 
-
-
-        //BOXES FOR MOUSE OVER
-        ageSvg.select("#mouseBoxes").selectAll(".mouseOverBox")
-            .data(leftData)
-            .enter()
-            .append("rect")
-                .attr("class","mouseOverBox")
-                .attr("opacity",0)
-                .attr("width",ageWidth)
-                .attr("height",ageScale.bandwidth())
-                .attr("y", function(d,i){ return ageScale(i)})
-                .on("mouseover", function(d){
-                    // console.log(d.age_group_name)
-                    ageSvg.selectAll((".barText"+d.ageCat))
-                        .attr("opacity",1)
-                })
-                .on("mouseout", function(d){
-                    // console.log(d.age_group_name)
-                    ageSvg.selectAll((".barText"+d.ageCat))
-                        .attr("opacity",0)
-                })
+            //BOXES FOR MOUSE OVER
+            ageSvg.select("#mouseBoxes").selectAll(".mouseOverBox")
+                .data(leftData)
+                .enter()
+                .append("rect")
+                    .attr("class","mouseOverBox")
+                    .attr("opacity",0)
+                    .attr("width",ageWidth)
+                    .attr("height",ageScale.bandwidth())
+                    .attr("y", function(d,i){ return ageScale(i)})
+                    .on("mouseover", function(d){
+                        // console.log(d.age_group_name)
+                        ageSvg.selectAll((".barText"+d.ageCat))
+                            .attr("opacity",1)
+                    })
+                    .on("mouseout", function(d){
+                        // console.log(d.age_group_name)
+                        ageSvg.selectAll((".barText"+d.ageCat))
+                            .attr("opacity",0)
+                    })
+            }
     })
 
 }
